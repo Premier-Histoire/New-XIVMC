834a65491d36cacae1695df7fa25396bc787e8c9
Premier/ぷるみえ <135576669+Premier-Histoire@users.noreply.github.com>
2024-05-13 11:16:05 +0900

    Update index.js

--- src/router/index.js
+++ src/router/index.js
@@ -11,10 +11,29 @@ const routes = [
   { path: '/name/:ItemName', component: Itemlist, props: true },
   // ルートパスの定義
   { 
-    path: '/:pathMatch(.*)*', // すべてのパスにマッチ
+    path: '/tag/:tagNumber', // /tag/ で始まるパスにマッチ
     redirect: (to) => {
-      // リダイレクト先のパスを動的に生成
-      return `/tag/${to.params.tagNumber || defaultTagNumber}`;
+      // パスが /tag/ で始まる場合、リダイレクトしない
+      return to.path;
+    }
+  },
+  {
+    path: '/name/:ItemName', // /name/ で始まるパスにマッチ
+    redirect: (to) => {
+      // パスが /name/ で始まる場合、リダイレクトしない
+      return to.path;
+    }
+  },
+  { 
+    path: '/:catchAll(.*)', // すべてのパスにマッチ
+    redirect: (to) => {
+      // パスが /tag/ または /name/ で始まる場合のみリダイレクト
+      if (to.path.startsWith('/tag/') || to.path.startsWith('/name/')) {
+        return to.path;
+      } else {
+        // それ以外の場合、デフォルトのパスにリダイレクト
+        return `/tag/${defaultTagNumber}`;
+      }
     }
   }
 ]
