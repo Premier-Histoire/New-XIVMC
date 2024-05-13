a272d25fbe7d05adf2c303a820e9019abd2a69f9
Premier/ぷるみえ <135576669+Premier-Histoire@users.noreply.github.com>
2024-05-13 11:10:28 +0900

    main

--- src/router/index.js
+++ src/router/index.js
@@ -1,10 +1,22 @@
 import { createRouter, createWebHistory } from 'vue-router'
+import App from './App.vue'
 import Itemlist from './components/Itemlist.vue'
+
+// デフォルトのパスを定義
+const defaultTagNumber = '1'; // デフォルトのタグ番号
 
 const routes = [
   { path: '/', component: App },
   { path: '/tag/:tagNumber', component: Itemlist, props: true },
   { path: '/name/:ItemName', component: Itemlist, props: true },
+  // ルートパスの定義
+  { 
+    path: '/:pathMatch(.*)*', // すべてのパスにマッチ
+    redirect: (to) => {
+      // リダイレクト先のパスを動的に生成
+      return `/tag/${to.params.tagNumber || defaultTagNumber}`;
+    }
+  }
 ]
 
 const router = createRouter({
