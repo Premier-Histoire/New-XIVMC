bcdf169e966d982d7c38a318812130b34a0a3437
Premier/ぷるみえ <135576669+Premier-Histoire@users.noreply.github.com>
2024-05-13 13:44:08 +0900

    main

--- src/router/index.js
+++ src/router/index.js
@@ -1,9 +1,7 @@
 import { createRouter, createWebHistory } from 'vue-router'
-import App from './App.vue'
 import Itemlist from './components/Itemlist.vue'
 
 const routes = [
-  { path: '/', component: App },
   { path: '/tag/:tagNumber', component: Itemlist, props: true },
   { path: '/name/:ItemName', component: Itemlist, props: true },
 ]
