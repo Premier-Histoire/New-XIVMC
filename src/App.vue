d6d351a702a81d578414107dde67f7ed6af3aa03
Premier/ぷるみえ <135576669+Premier-Histoire@users.noreply.github.com>
2024-05-13 10:50:57 +0900

    Update App.vue

--- src/App.vue
+++ src/App.vue
@@ -2,16 +2,17 @@
   <div id="app">
     <Header />
     <div class="main-content" :class="{ 'mobile-layout': isMobile }">
-      <div class="left-content">
+      <div class="left-content" :class="{ 'hide': isMobile && !rightContentVisible }">
         <div class="item-search">
           <h5>検索結果</h5>
         </div>
         <div class="scrollable-area">
-          <Itemlist class="item-list"></Itemlist>
+          <Itemlist class="item-list" @item-clicked="ItemListClick"></Itemlist>
         </div>
       </div>
-      <div class="right-content">
-        <Detail />
+      <div class="right-content" :class="{ 'hide': isMobile && rightContentVisible }">
+        <Detail v-show="!isMobile" />
+        <MobileDetail v-show="isMobile" />
       </div>
     </div>
   </div>
@@ -21,16 +22,22 @@
 import Header from './components/Header.vue';
 import Itemlist from './components/Itemlist.vue';
 import Detail from './components/Detail.vue';
+import MobileDetail from './components/MobileDetail.vue';
+import { mapState, mapActions } from 'vuex';
 
 export default {
   components: {
     Header,
     Itemlist,
-    Detail
+    Detail,
+    MobileDetail,
+  },
+  computed: {
+    ...mapState(['rightContentVisible']),
   },
   data() {
     return {
-      isMobile: false
+      isMobile: false,
     };
   },
   mounted() {
@@ -40,20 +47,16 @@ export default {
   destroyed() {
     window.removeEventListener('resize', this.checkIsMobile);
   },
-  watch: {
-    '$route.params.tagName': function (newValue, oldValue) {
-      if (newValue !== oldValue) { // タグの名前が変更された場合に再検索を行う
-        this.scrollToTop(); // スクロール位置を一番上に戻す
-      }
-    }
-  },
   methods: {
+    ...mapActions(['toggleRightContentVisibility']),
     checkIsMobile() {
-      this.isMobile = window.innerWidth <= 768; // 768px以下の場合をスマートフォンと見なす
+      this.isMobile = window.innerWidth <= 768;
     },
-    scrollToTop() {
-      this.$el.querySelector('.scrollable-area').scrollTop = 0; // スクロール可能領域のscrollTopを0に設定してスクロール位置を一番上に戻す
-    }
+    ItemListClick() {
+      if (this.isMobile) {
+        this.toggleRightContentVisibility();
+      }
+    },
   },
 };
 </script>
@@ -62,7 +65,6 @@ export default {
 .main-content {
   position: absolute;
   top: 64px;
-  /* ヘッダーの高さ分下にずらす */
   left: 0;
   width: 100%;
   bottom: 0;
@@ -73,21 +75,18 @@ export default {
   width: 25%;
   float: left;
   height: calc(100vh - 64px);
-  /* ヘッダーの高さを引いた値を高さに設定 */
   padding: 10px;
 }
 
 .item-search {
   display: flex;
-  align-items: center; /* 縦方向（上下）の中央揃え */
+  align-items: center;
 }
 
 .scrollable-area {
   width: 100%;
-  /* 左右の余白を削除してスクロール可能領域の幅を設定 */
   height: calc(100% - 40px);
   overflow-y: auto;
-  /* スクロール可能にする */
 }
 
 .right-content {
@@ -95,25 +94,23 @@ export default {
   height: calc(100vh - 64px);
   float: left;
   border-left: 1px solid #d0d7de;
-  /* 左側に境界線を追加 */
 }
 
 .mobile-layout {
   flex-direction: column;
-  /* Mobileの場合、上下にレイアウト */
 }
 
+.mobile-layout .right-content,
 .mobile-layout .left-content {
   width: 100%;
   border-left: none;
-  /* モバイルの場合は境界線を削除 */
-}
-
-.mobile-layout .right-content {
-  display: none; /* モバイルの場合は右側のコンテンツエリアを非表示にする */
 }
 
 .item-list {
   width: 100%;
+}
+
+.hide {
+  display: none;
 }
 </style>
