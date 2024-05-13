ee3c067d8e0932780188a09a49663d5a0e5d2d1c
Premier/ぷるみえ <135576669+Premier-Histoire@users.noreply.github.com>
2024-05-13 11:30:03 +0900

    main

--- src/router/index.js
+++ src/router/index.js
@@ -3,24 +3,11 @@ import Itemlist from './components/Itemlist.vue'
 
 const routes = [
   { path: '/', component: App },
-  // リダイレクト先に変数を渡してリダイレクトする
-  { path: '/old-route/:oldParam', redirect: to => {
-      if (to.params.oldParam.startsWith('tag')) {
-        const tagNumber = to.params.oldParam.split('tag')[1];
-        return { path: `/tag/${tagNumber}` };
-      } else if (to.params.oldParam.startsWith('name')) {
-        const ItemName = to.params.oldParam.split('name')[1];
-        return { path: `/name/${ItemName}` };
-      } else {
-        // リダイレクト先が不明な場合はトップページにリダイレクト
-        return { path: '/' };
-      }
-    }
-  },
-  // その他のルート
   { path: '/tag/:tagNumber', component: Itemlist, props: true },
   { path: '/name/:ItemName', component: Itemlist, props: true },
-  { path: '/:pathMatch(.*)', redirect: '/' }, // 未定義のルートへのリダイレクト
+  // 追加のルート
+  { path: '/tag/', redirect: '/' },
+  { path: '/name/', redirect: '/' },
 ]
 
 const router = createRouter({
