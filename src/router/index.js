2887108f0b2f02c9a62a71a8b21708c590a9257a
Premier/ぷるみえ <135576669+Premier-Histoire@users.noreply.github.com>
2024-05-13 11:22:36 +0900

    Update index.js

--- src/router/index.js
+++ src/router/index.js
@@ -1,31 +1,17 @@
 import { createRouter, createWebHistory } from 'vue-router'
-import App from './App.vue'
 import Itemlist from './components/Itemlist.vue'
-
-// デフォルトのタグ番号
-const defaultTagNumber = '1';
 
 const routes = [
   { path: '/', component: App },
   { path: '/tag/:tagNumber', component: Itemlist, props: true },
   { path: '/name/:ItemName', component: Itemlist, props: true },
-  // その他のパスへのリダイレクトを処理
-  {
-    path: '/:catchAll(.*)', // すべてのパスにマッチ
-    redirect: (to) => {
-      // リダイレクト先のパスを動的に生成
-      if (to.params.tagNumber) {
-        // /tag/パスにアクセスされ、かつtagNumberが指定されている場合
-        return `/tag/${to.params.tagNumber}`;
-      } else if (to.params.ItemName) {
-        // /name/パスにアクセスされ、かつItemNameが指定されている場合
-        return `/name/${to.params.ItemName}`;
-      } else {
-        // それ以外の場合はデフォルトのパスにリダイレクト
-        return `/tag/${defaultTagNumber}`;
-      }
+  // リダイレクトを追加
+  { path: '/old-route/:oldParam', redirect: to => {
+      return { path: `/tag/${to.params.oldParam}` }
     }
-  }
+  },
+  // 未定義のルートへのリダイレクト
+  { path: '/:pathMatch(.*)', redirect: '/' },
 ]
 
 const router = createRouter({
